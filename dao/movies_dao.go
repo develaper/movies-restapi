9d103f7cc1f2d07156bb2991ca0477b8bbec0f66
develaper <lachguer.brahim@gmail.com>
2018-09-06 19:10:05 +0200

    Adds DAO and stablish connection

---
+++ dao/movies_dao.go
@@ -0,0 +1,28 @@
+package dao
+
+import (
+	"log"
+
+	"github.com/develaper/movies-restapi/models"
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type MoviesDAO struct {
+	Server   string
+	Database string
+}
+
+var db *mgo.Database
+
+const (
+	COLLECTION = "movies"
+)
+
+func (m *MoviesDAO) Connect() {
+	session, err := mgo.Dial(m.Server)
+	if err != nil {
+		log.Fatal(err)
+	}
+	db = session.DB(m.Database)
+}
