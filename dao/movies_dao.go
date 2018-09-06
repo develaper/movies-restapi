8a9cbecd0bfd52cdd9cc390a0f9740e0fcdac30d
develaper <lachguer.brahim@gmail.com>
2018-09-06 19:22:52 +0200

    Adds db queries

--- dao/movies_dao.go
+++ dao/movies_dao.go
@@ -26,3 +26,30 @@ func (m *MoviesDAO) Connect() {
 	}
 	db = session.DB(m.Database)
 }
+
+func (m *MoviesDAO) FindAll() ([]Movie, error) {
+	var movies []Movie
+	err := db.C(COLLECTION).Find(bson.M{}).All(&movies)
+	return movies, err
+}
+
+func (m *MoviesDAO) FindById(id string) (Movie, error) {
+	var movie Movie
+	err := db.C(COLLECTION).FindId(bson.ObjectIdHex(id)).One(&movie)
+	return movie, err
+}
+
+func (m *MoviesDAO) Insert(movie Movie) error {
+	err:= db.C(COLLECTION).Insert(&movie)
+	return err
+}
+
+func (m *MoviesDAO) Delete(movie Movie) error {
+	err:= db.C(COLLECTION).Remove(&movie)
+	return err
+}
+
+func (m *MoviesDAO) Update(movie Movie) error {
+	err:= db.C(COLLECTION).UpdateId(movie.ID, &movie)
+	return err
+}
