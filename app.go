223b6d8b7d6d0f27db575c4746fcc8ab6e472170
develaper <lachguer.brahim@gmail.com>
2018-09-06 19:58:55 +0200

    Adds UpdateMovieEndPoint

--- app.go
+++ app.go
@@ -48,7 +48,17 @@ func CreateMovieEndPoint(w http.ResponseWriter, r *http.Request) {
 
 
 func UpdateMovieEndPoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "not implemented yet !")
+	defer r.Body.Close()
+	var movie Movie
+	if err := json.NewDecoder(r.Body).Decode(&movie); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	if err := dao.Update(movie); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusOK, map[string]string{"result": "success"})
 }
 
 func DeleteMovieEndPoint(w http.ResponseWriter, r *http.Request) {
